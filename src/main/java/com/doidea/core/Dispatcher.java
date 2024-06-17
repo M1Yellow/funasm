bd763e9d1e635a0784c847c54445303833940c37
M1Yellow <m1yellow@163.com>
2024-06-18 02:16:24 +0800

    添加说明

--- src/main/java/com/doidea/core/Dispatcher.java
+++ src/main/java/com/doidea/core/Dispatcher.java
@@ -67,17 +67,18 @@ public class Dispatcher implements ClassFileTransformer {
     public byte[] transform(ClassLoader loader, String className, Class<?> classBeingRedefined, ProtectionDomain protectionDomain, byte[] classfileBuffer) throws IllegalClassFormatException {
         if (null == className || className.isBlank()) return classfileBuffer;
         try {
-            // TODO transform 方法内的日志能在 idea.log 中打印
+            // TODO transform 方法内的日志可以在 idea.log 中打印（用 everything 搜索这个日志文件）
             //System.out.println(">>>> Transform Class: " + className); // xxx/xxxx/xxx 格式
             String targetClassName = className.replace("/", "."); // targetClass 为 xxx.xxxx.xxx$xxx 格式
             List<IMyTransformer> transformers = this.transformerMap.get(targetClassName);
             if (null == transformers || transformers.isEmpty()) return classfileBuffer;
+
             // 命中目标类
             System.out.println(">>>> Target Class: " + className);
             System.out.println(">>>> Original ClassLoader: " + loader);
             // TODO may be null if the bootstrap loader 启动类加载器加载的核心类，loader 为 null。比如：java.lang.String、java.awt.Dialog
-            if (null == loader)
-                loader = ClassLoader.getSystemClassLoader().getParent(); // jdk.internal.loader.ClassLoaders$PlatformClassLoader
+            if (null == loader) loader = ClassLoader.getSystemClassLoader().getParent();
+            // ClassLoader.getSystemClassLoader().getParent() -> jdk.internal.loader.ClassLoaders$PlatformClassLoader
             System.out.println(">>>> Target ClassLoader: " + loader.toString()); // toString() -> PathClassLoader；getName() -> null
 
             // 插件运行模式
@@ -85,17 +86,16 @@ public class Dispatcher implements ClassFileTransformer {
                 System.out.println(">>>> Current MODE: " + Launcher.propMap.get("mode"));
 
             int order = 0; // 自定义 transform 执行顺序，暂未使用
-
             for (IMyTransformer transformer : transformers) {
                 classfileBuffer = transformer.transform(loader, classBeingRedefined, protectionDomain, className, classfileBuffer, order++);
-                // TODO 额外处理 ASM 在 transform 阶段没有处理的类转换，依附在一个能处理的类后面执行【没有生效！后续再看】
+                // TODO 额外处理 ASM 在 transform 阶段没有处理的类转换，依附在一个能处理的类后面执行
                 if (transformer.getTargetClassName().equals("sun.management.VMManagementImpl")) {
                     System.out.println(">>>> ASM doExtendTransform >>>>");
                     for (Class<?> c : inst.getAllLoadedClasses()) { // 截至 VMManagementImpl 类加载时，IDEA 应用层面的类还没加载
                         String name = c.getName().replace("/", ".");
                         if (classSet.contains(name)) System.out.println(">>>> Instrumentation loaded class: " + name);
                     }
-                    new ExtendTransformer().doExtendTransform(inst);
+                    new ExtendTransformer().doExtendTransform(inst); // 【验证有效】
                 }
             }
         } catch (Throwable e) {
